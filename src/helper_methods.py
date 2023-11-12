d7fe4c6a515aaf129ec71151d6e238e21f6bb04e
Abey <abrahamborg@gmail.com>
2023-11-12 08:32:25 -0800

    removed long version of agent_v_agent need to use nested functions more often

--- src/helper_methods.py
+++ src/helper_methods.py
@@ -40,54 +40,7 @@ def play_game(bubs: Bradley.Bradley, rl_agent_color: str) -> None:
         bubs.reset_environ()
 ### end of play_game
 
-def agent_vs_agent(bubs: Bradley.Bradley) -> None:
-    """Play two trained agents against each other.
-    Args:
-        bubs: An object of the `Bradley` class representing the chess game environment.
-    """    
-    agent_vs_agent_file = open(game_settings.agent_vs_agent_filepath, 'a')
 
-    while bubs.is_game_on():
-        try:        
-            agent_vs_agent_file.write(f'\nCurrent turn: {bubs.get_curr_turn()}')
-        except Exception as e:
-            agent_vs_agent_file.write(f'An error occurred: {e}')
-            raise Exception from e
-
-        try:
-            chess_move_bubs: str = bubs.rl_agent_selects_chess_move('W')
-        except Exception as e:
-            agent_vs_agent_file.write(f'An error occurred: {e}')
-            raise Exception from e
-
-        agent_vs_agent_file.write(f'Bubs played {chess_move_bubs}\n')
-
-        # imman's turn, check for end of game again, since the game could have ended after W's move.
-        if bubs.is_game_on():
-            agent_vs_agent_file.write(f'Current turn:  {turn_num}')
-            try:
-                chess_move_imman: str = bubs.rl_agent_selects_chess_move('B')
-            except Exception as e:
-                agent_vs_agent_file.write(f'An error occurred: {e}')
-                raise Exception from e
-        
-        agent_vs_agent_file.write(f'Imman played {chess_move_imman}\n')
-            
-    agent_vs_agent_file.write('Game is over, chessboard looks like this:\n')
-    agent_vs_agent_file.write(bubs.get_chessboard())
-    agent_vs_agent_file.write('\n\n')
-    try:
-        agent_vs_agent_file.write(f'Game result is: {bubs.get_game_outcome()}')
-    except Exception as e:
-        agent_vs_agent_file.write(f'An error occurred at get_game_outcome: {e}')
-
-    try:
-        agent_vs_agent_file.write(f'Game ended because of: {bubs.get_game_termination_reason()}')
-    except Exception as e:
-        agent_vs_agent_file.write(f'An error occurred at get_game_termination_reason: {e}')
-
-    bubs.reset_environ()
-    agent_vs_agent_file.close()
 ### end of agent_vs_agent
 
 def pikl_q_table(bubs: Bradley.Bradley, rl_agent_color: str, q_table_path: str) -> None:
