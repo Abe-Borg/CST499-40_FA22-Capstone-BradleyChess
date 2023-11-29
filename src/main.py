7a8706a3b820ff2e6fa22bf77377d246f05914a0
Abey <abrahamborg@gmail.com>
2023-11-28 18:24:36 -0800

    removing corrupted games

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     
 
     # ========== IDENTIFY AND REMOVE CORRUPTED GAMES FROM CHESS DATABASE ==========
-    chess_data_file_path = game_settings.chess_pd_dataframe_file_path_part_11_part_1
+    chess_data_file_path = game_settings.chess_pd_dataframe_file_path_part_11_part_10
 
     chess_data = pd.read_pickle(chess_data_file_path, compression = 'zip')
 
